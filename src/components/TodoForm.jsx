94311a612465e889fdf616df4412911e5c91abfb
NHeroOP <neeravtheheroo@gmail.com>
2024-02-26 23:41:10 +0530

    Update many things

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,20 +1,25 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { addTodo } from '../features/Todo/todoSlice'
+import { addTodo } from "../app/todoSlice"
 
 export default function AddTodo() {
   const [input, setInput] = useState("")
+  const [check, setIsChecked] = useState(false)
   const dispatch = useDispatch()
 
   const addTodoHandler = (e) => {
     e.preventDefault()
     dispatch(addTodo(input))
     setInput("")
+    console.log("test");
   }
 
   return (
-    <form onSubmit={addTodoHandler}>
-      <input type="checkbox"></input>
+    <form onSubmit={addTodoHandler} id='AddTodoForm'>
+      <input type="checkbox" onChange={(e) => {
+        setIsChecked(e.target.checked)
+        if(e.target.checked) addTodoHandler(e)
+      } }></input>
       <input
         type="text"
         placeholder="Create a new Todo..."
